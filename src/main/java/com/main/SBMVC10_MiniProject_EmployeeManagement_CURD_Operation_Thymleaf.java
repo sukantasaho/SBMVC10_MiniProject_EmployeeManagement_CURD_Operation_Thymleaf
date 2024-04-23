04699519b0eceae4ecebf7695c7301154438956b
sukantasaho <ssukanta520@gmail.com>
2024-04-23 17:15:18 +0530

    commit

---
+++ src/main/java/com/main/SBMVC10_MiniProject_EmployeeManagement_CURD_Operation_Thymleaf.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBMVC10_MiniProject_EmployeeManagement_CURD_Operation_Thymleaf {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBMVC10_MiniProject_EmployeeManagement_CURD_Operation_Thymleaf.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/SBMVC10_MiniProject_EmployeeManagement_CURD_Operation_ThymleafTests.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBMVC10_MiniProject_EmployeeManagement_CURD_Operation_ThymleafTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
